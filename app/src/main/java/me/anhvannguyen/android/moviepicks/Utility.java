9720fd5dfff7bcac8d54c12fec7fdef5731aee4c
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-13 17:25:45 -0700

    add helper to convert string date to java date

--- app/src/main/java/me/anhvannguyen/android/moviepicks/Utility.java
+++ app/src/main/java/me/anhvannguyen/android/moviepicks/Utility.java
@@ -4,13 +4,31 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by anhvannguyen on 6/13/15.
  */
 public class Utility {
+
     public static String getSortingPreference(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_sortorder_key),
                 context.getString(R.string.pref_sort_value_popular));
+    }
+
+    public static Date getDateFromString(String dateString) {
+        Date date = null;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            date = dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
     }
 }
