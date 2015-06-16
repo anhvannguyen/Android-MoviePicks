e14f7bf261fd0a2e50649afe82b68c35d1f21b11
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-16 15:10:44 -0700

    create uri matcher

--- app/src/main/java/me/anhvannguyen/android/moviepicks/data/MovieContentProvider.java
+++ app/src/main/java/me/anhvannguyen/android/moviepicks/data/MovieContentProvider.java
@@ -2,6 +2,7 @@ package me.anhvannguyen.android.moviepicks.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,18 +10,46 @@ import android.net.Uri;
  * Created by anhvannguyen on 6/16/15.
  */
 public class MovieContentProvider extends ContentProvider {
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mOpenHelper;
+
+    private static final int MOVIES = 100;
+    private static final int MOVIES_WITH_ID = 101;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieDbContract.CONTENT_AUTHORITY;
+
+        uriMatcher.addURI(authority, MovieDbContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(authority, MovieDbContract.PATH_MOVIES + "/#", MOVIES_WITH_ID);
+
+        return uriMatcher;
+
+    }
+
     @Override
     public boolean onCreate() {
         return false;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+    public String getType(Uri uri) {
+        // Use the Uri Matcher to determine what kind of URI this is.
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIES:
+                return MovieDbContract.MovieEntry.CONTENT_TYPE;
+            case MOVIES_WITH_ID:
+                return MovieDbContract.MovieEntry.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Override
-    public String getType(Uri uri) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         return null;
     }
 
