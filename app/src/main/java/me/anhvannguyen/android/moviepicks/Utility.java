d8c857717d4a7a611b42bf83116147cf22a25509
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-18 16:41:54 -0700

    create helper to get image path

--- app/src/main/java/me/anhvannguyen/android/moviepicks/Utility.java
+++ app/src/main/java/me/anhvannguyen/android/moviepicks/Utility.java
@@ -12,11 +12,16 @@ import java.util.Date;
  * Created by anhvannguyen on 6/13/15.
  */
 public class Utility {
+    public static String MDB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
     public static int getSortingPreference(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return Integer.parseInt(prefs.getString(context.getString(R.string.pref_sortorder_key),
                 context.getString(R.string.pref_sort_value_popular)));
+    }
+
+    public static String getFullImagePath(String size, String posterPath) {
+        return MDB_IMAGE_BASE_URL + size + posterPath;
     }
 
     public static Date getDateFromString(String dateString) {
