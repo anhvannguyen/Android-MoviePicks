e0d3edfe0decaeb5a581742aa809e7024a974d5b
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-18 16:42:28 -0700

    bind the data to view of the cursor adapter

--- app/src/main/java/me/anhvannguyen/android/moviepicks/MovieCursorAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/moviepicks/MovieCursorAdapter.java
@@ -9,6 +9,10 @@ import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import me.anhvannguyen.android.moviepicks.data.MovieDbContract;
+
 /**
  * Created by anhvannguyen on 6/18/15.
  */
@@ -23,12 +27,45 @@ public class MovieCursorAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.list_item_movie, parent, false);
 
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
 
+        // Get the url of the poster path
+        int posterIndex = cursor.getColumnIndex(MovieDbContract.MovieEntry.COLUMN_POSTER_PATH);
+        String posterPath = cursor.getString(posterIndex);
+        String fullPosterPath = Utility.getFullImagePath(
+                context.getString(R.string.image_poster_w154),
+                posterPath
+        );
+        // Set the imageview with the poster image
+        Picasso.with(context)
+                //.setIndicatorsEnabled(true)
+                .load(fullPosterPath)
+                //.error(R.mipmap.ic_launcher)
+                .into(viewHolder.mPosterImage);
+
+        // Set the title textview
+        int titleIndex = cursor.getColumnIndex(MovieDbContract.MovieEntry.COLUMN_TITLE);
+        viewHolder.mTitleTextView.setText(cursor.getString(titleIndex));
+
+        // Set the rating textview
+        int voteAverageIndex = cursor.getColumnIndex(MovieDbContract.MovieEntry.COLUMN_VOTE_AVERAGE);
+        int voteCountIndex = cursor.getColumnIndex(MovieDbContract.MovieEntry.COLUMN_VOTE_COUNT);
+        Double movieRating = cursor.getDouble(voteAverageIndex);
+        int movieVoteCount = cursor.getInt(voteCountIndex);
+        viewHolder.mRatingTextView.setText("Rating: " + movieRating +
+                " (" + movieVoteCount + ")");
+
+        // Set the date textview
+        int releaseDateIndex = cursor.getColumnIndex(MovieDbContract.MovieEntry.COLUMN_RELEASE_DATE);
+        viewHolder.mReleaseDateTextView.setText(cursor.getString(releaseDateIndex));
     }
 
     public static class ViewHolder {
