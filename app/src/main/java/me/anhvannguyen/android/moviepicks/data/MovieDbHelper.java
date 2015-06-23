2a5704c5806863f7f24543d1fb0be99758964296
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-06-22 20:53:29 -0700

    create the trailer db table

--- app/src/main/java/me/anhvannguyen/android/moviepicks/data/MovieDbHelper.java
+++ app/src/main/java/me/anhvannguyen/android/moviepicks/data/MovieDbHelper.java
@@ -33,8 +33,21 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                 MovieDbContract.MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL," +
                 MovieDbContract.MovieEntry.COLUMN_FAVORITE + " INTEGER NOT NULL DEFAULT 0" +
                 " );";
+        final String SQL_CREATE_TRAILER_TABLE = "CREATE TABLE " + MovieDbContract.TrailerEntry.TABLE_NAME + " (" +
+                MovieDbContract.TrailerEntry._ID + " INTEGER PRIMARY KEY," +
+                MovieDbContract.TrailerEntry.COLUMN_MDB_ID + " INTEGER NOT NULL," +
+                MovieDbContract.TrailerEntry.COLUMN_TRAILER_ID + " TEXT NOT NULL," +
+                MovieDbContract.TrailerEntry.COLUMN_KEY + " TEXT NOT NULL," +
+                MovieDbContract.TrailerEntry.COLUMN_NAME + " TEXT NOT NULL," +
+                MovieDbContract.TrailerEntry.COLUMN_SITE + " TEXT NOT NULL," +
+                MovieDbContract.TrailerEntry.COLUMN_TYPE + " TEXT NOT NULL," +
+
+                " UNIQUE (" + MovieDbContract.TrailerEntry.COLUMN_TRAILER_ID + ") ON CONFLICT REPLACE" +
+                " );";
+
 
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
+        db.execSQL(SQL_CREATE_TRAILER_TABLE);
     }
 
     @Override
